ae92f5c641a3d40565ba168cb5ea987489b7060e
harishganapathi <harish.k@caratlane.com>
2023-05-27 01:44:46 +0530

    IC

---
+++ worker.js
@@ -0,0 +1,23 @@
+addEventListener('fetch', event => {
+    var url = new URL(event.request.url);
+
+    if (url.pathname.startsWith('/blog') || url.pathname.startsWith('/_elderjs')) {
+        handleBlog(event, url);
+    } else {
+        event.respondWith(fetch(event.request));
+    }
+})
+
+async function handleBlog(event, url) {
+    console.log("event", url);
+    // var originUrl = url.toString().replace(
+    //     "https://wizmakers.com/_elderjs",
+    //     'https://dailywriter.superblog.cloud/_elderjs');
+
+    // originUrl =
+    //     originUrl.toString().replace(
+    //         "https://wizmakers.com/blog",
+    //         "https://dailywriter.superblog.cloud");
+
+    event.respondWith(fetch(originUrl));
+}
